bc6e641239d264be25398b640b1a9d47a7d5e94c
David <DavidLegrand@users.noreply.github.com>
2021-04-12 17:02:01 +0200

    Added props

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,23 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Badge, ListGroupItem } from "react-bootstrap";
+import { Badge, Button, ListGroupItem } from "react-bootstrap";
 import TaskModel from 'models/Task'
 
-const Task = () => {
-  const task = new TaskModel({
-    title: "Nouvelle tâche",
-    description: "Ceci est une tâche test"
-  })
-  return <ListGroupItem variant={task.getVariant()}>
-    <h2 className="d-inline">{task.title}</h2>
-    <Badge className="float-right" variant={task.getVariant()}>{task.getStatus()}</Badge>
-    <p>Deadline : {task.deadline.toLocaleDateString()}</p>
-    <p>Temps restant : {task.getRemaining()} jours</p>
-  </ListGroupItem>;
+const Task = ({ update, task }) => {
+
+
+  return (
+    <ListGroupItem variant={task.getVariant()}>
+
+      <h2 className="d-inline">
+        {task.title}
+      </h2>
+
+      <Badge className="float-right" variant={task.getVariant()}>
+        {task.getStatus()}
+      </Badge>
+
+      <p>{task.description}</p>
+      <p>Deadline : {task.deadline.toLocaleDateString()}</p>
+      <p>Temps restant : {task.getRemaining()} jours</p>
+
+      <Button onClick={() => update(task)}>Terminer</Button>
+      <Button onClick={() => update(task)}>Annuler</Button>
+
+    </ListGroupItem>
+  )
 };
 
 Task.propTypes = {
-  //
+  update: PropTypes.func.isRequired,
+  task: PropTypes.instanceOf(TaskModel).isRequired
 };
 
 export default Task;
--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -3,11 +3,24 @@ import PropTypes from "prop-types";
 import { ListGroup } from "react-bootstrap";
 import Task from "components/Task";
 
+import TaskModel from 'models/Task'
+
 const ToDoList = () => {
+
+  // Simulation de récupération API
+  const tasks = [
+    { id: 1, title: "Faire marcher la list", description: "Arriver à afficher cette liste grâce à un map", completed: true },
+    { id: 2, title: "Comprendre les props", description: "Comprendre les tenants et les aboutissants des props" },
+    { id: 3, title: "Envoyer une fonction en props", description: "Arriver à déclencher dans Task une fonciton envoyée par ToDoList", completed: true },
+  ]
+
+  // Logique métier mettre à jour le statut completed de la tâche
+  const updateCompleted = (task) => {
+    console.log(task)
+  }
+
   return <ListGroup>
-    <Task></Task>
-    <Task></Task>
-    <Task></Task>
+    {tasks.map((task) => <Task key={task.id} task={new TaskModel(task)} update={updateCompleted}></Task>)}
   </ListGroup>;
 };
 
